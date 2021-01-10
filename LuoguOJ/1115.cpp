85729343c19498b553f100914bf663696cb5525f
Luo Youxi <immccn123@outlook.com>
2021-01-10 15:49:00 +0800

    Finished P1115

--- LuoguOJ/1115.cpp
+++ LuoguOJ/1115.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 using namespace std;
+int max(int a,int b)
+{
+	if(a > b) return a;
+	else return b;
+}
+
 int main()
 {
-	int n;
-	cin >> n;
-	int list[n + 1];
-	for(int i = 0;i < n;i++)
-	{
-		cin >> list[i];
-	}
-	
+    int n[200001], p, lst[200001]={0};
+    int sum=-0xc3c3c3;
+    cin >> p;
+    for(int i=1;i<=p;i++)
+    {
+        cin >> n[i];
+        lst[i] = max(lst[i-1] + n[i],n[i]);
+        sum = max( sum, lst[i]);
+    }
+    cout << sum;
+    return 0;
 }
