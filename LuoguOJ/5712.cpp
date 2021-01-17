5c3687db431b6a713be2fdbff0caec3c0395312a
Luo Youxi <immccn123@outlook.com>
2021-01-17 16:49:26 +0800

    Finished:5712,5713,5722

---
+++ LuoguOJ/5712.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int apple;
+	cin >> apple;
+	if(apple == 0) cout << "Today, I ate 0 apple.";
+	else if(apple == 1) cout << "Today, I ate 1 apple.";
+	else cout << "Today, I ate " << apple << " apples.";
+	return 0;
+}
---
+++ LuoguOJ/5713.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	if(n > 5.5) cout << "Luogu";
+	else cout << "Local";
+	return 0;
+}
